8483a021c4de06747b83dd324d608cb3df073b69
Praveen Kumar Thakur <pravee1701@gmail.com>
2024-07-12 13:20:53 +0530

    created portfolio

---
+++ app/provider.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-theme"
+import { type ThemeProviderProps } from "next-theme/dist/provider/index.props"
+
+export function ThemeProvider({children, ...props}: ThemeProviderProps){
+    return (<NextThemesProvider {...props}>{children}</NextThemesProvider>
+    );
+}
